1aec23d84859007691ecea87e918188f4a1e3221
matteo.faccetta.stud <matteo.faccetta.stud@ispascalcomandini.it>
2024-10-28 14:49:53 +0100

    sistemati errorini

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types; 
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:uuid/uuid.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/services.dart' show rootBundle;
@@ -14,23 +14,22 @@ void main() {
   );
 }
 
-class SimpleChat extends StatelessWidget{
-  const SimpleChat({super.key})
+class SimpleChat extends StatelessWidget {
+  const SimpleChat({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Faccettz Chat",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          useMaterial3: true),
       home: const MyHomePage(title: 'chat'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -39,19 +38,33 @@ class MyHomePage extends StatefulWidget{
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
   List<types.Message> _messages = [];
   final _user = const types.User(id: 'aaaa');
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _loadMessages();
+    //_loadMessages();
   }
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold()
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Chat(
+        messages: _messages,
+        onSendPressed: _handleSendPress,
+        user: _user,
+        onPreviewDataFetched: _handlePreviewDataFetched,
+        showUserAvatars: true,
+        showUserNames: true,
+        theme: const DefaultChatTheme(
+            seenIcon: Text('read', style: TextStyle(fontSize: 10.0))),
+      ),
+    );
   }
-}
 
+  void _handleSendPress(types.PartialText p1) {}
+
+  void _handlePreviewDataFetched(types.TextMessage p1, types.PreviewData p2) {}
+}
