58e7d0b9bcb7e5731f2b7f207f671a37cd94c665
Matteo <matteo.faccetta.stud@ispascalcomandini.it>
2025-02-08 09:05:26 +0100

    aggiornato server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io"); //si basa su http
+const firebase = require("firebase-admin");
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || "0.0.0.0";
 
 const io = socketIo(server, {
   cors: {
@@ -19,10 +21,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("Client disconnesso");
   });
-  socket.on("join-room", (room) => {
+  socket.on("join-room", (room, host) => {
     socket.leave(currentRoom);
     socket.join(room);
-    console.log(`Connesso alla stanza ${room}.`);
+    console.log(`${host} si è connesso alla stanza ${room}.`);
     currentRoom = room;
   });
 
@@ -39,6 +41,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, "192.168.0.124", () => {
+server.listen(PORT, IP, () => {
   console.log("server in ascolto alla porta ", PORT);
 });
